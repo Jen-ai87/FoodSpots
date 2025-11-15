a4c8210a59e389452633fe4264d86e8802a9e266
Faraz S <63739465+Fraz23@users.noreply.github.com>
2025-11-15 12:04:32 -0500

    Implement RestaurantManager singleton

--- app/src/main/java/com/example/foodspots/data/RestaurantManager.java
+++ app/src/main/java/com/example/foodspots/data/RestaurantManager.java
@@ -1,4 +1,23 @@
 package com.example.foodspots.data;
 
+import com.example.foodspots.R;
+import com.example.foodspots.models.Restaurant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;models.Restaurant;
 public class RestaurantManager {
-}
+    private static RestaurantManager instance;
+    private List<Restaurant> restaurants;
+    private int nextId = 7;
+
+    private RestaurantManager() {
+        restaurants = new ArrayList<>();
+        initializeDummyData();
+    }
+
+    public static RestaurantManager getInstance() {
+        if (instance == null) {
+            instance = new RestaurantManager();
+        }
+        return instance;
+    }
