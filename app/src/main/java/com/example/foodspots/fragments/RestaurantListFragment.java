8081d3d4b6d33107f7a42bf32b1079966d2b0850
Faraz S <63739465+Fraz23@users.noreply.github.com>
2025-11-15 12:14:37 -0500

    Complete RestaurantListFragment

--- app/src/main/java/com/example/foodspots/fragments/RestaurantListFragment.java
+++ app/src/main/java/com/example/foodspots/fragments/RestaurantListFragment.java
@@ -24,4 +24,61 @@ public class RestaurantListFragment extends Fragment {
     private RecyclerView recyclerView;
     private RestaurantAdapter adapter;
     private FloatingActionButton fab;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_restaurant_list, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        fab = view.findViewById(R.id.fab_add);
+
+        setupRecyclerView();
+        setupFab();
+
+        return view;
+    }
+
+    private void setupRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new RestaurantAdapter(getContext(),
+                RestaurantManager.getInstance().getAllRestaurants());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setupFab() {
+        fab.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AddEditRestaurantActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapter.updateData(RestaurantManager.getInstance().getAllRestaurants());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.list_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            Intent intent = new Intent(getActivity(), SearchActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
