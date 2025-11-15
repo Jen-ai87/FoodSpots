5369bf4456d2419e45589b3eeb3992cf18de9368
Faraz S <63739465+Fraz23@users.noreply.github.com>
2025-11-15 12:08:21 -0500

    Create basic RestaurantAdapter with ViewHolder

--- app/src/main/java/com/example/foodspots/adapters/RestaurantAdapter.java
+++ app/src/main/java/com/example/foodspots/adapters/RestaurantAdapter.java
@@ -1,4 +1,27 @@
 package com.example.foodspots.adapters;
 
-public class RestaurantAdapter {
-}
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.foodspots.R;
+import com.example.foodspots.RestaurantDetailActivity;
+import com.example.foodspots.models.Restaurant;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+import java.util.List;
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> {
+    private List<Restaurant> restaurants;
+    private Context context;
+
+    public RestaurantAdapter(Context context, List<Restaurant> restaurants) {
+        this.context = context;
+        this.restaurants = restaurants;
+    }
