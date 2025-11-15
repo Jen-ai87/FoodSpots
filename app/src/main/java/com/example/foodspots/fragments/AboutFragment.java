73646cadcec584063e728cc96d1ed3df4d84d8d9
Faraz S <63739465+Fraz23@users.noreply.github.com>
2025-11-15 12:12:27 -0500

    Create MapFragment

--- app/src/main/java/com/example/foodspots/fragments/AboutFragment.java
+++ app/src/main/java/com/example/foodspots/fragments/AboutFragment.java
@@ -8,5 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.foodspots.R;
+
 public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
 }
--- app/src/main/java/com/example/foodspots/fragments/MapFragment.java
+++ app/src/main/java/com/example/foodspots/fragments/MapFragment.java
@@ -1,4 +1,23 @@
 package com.example.foodspots.fragments;
 
-public class MapFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.foodspots.R;
+import com.example.foodspots.data.RestaurantManager;
+import com.example.foodspots.models.Restaurant;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.List;
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap googleMap;
 }
